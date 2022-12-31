1f9034bff78ca8cd168701d6052144d01039bea2
Kishor  Desai <114481961+GitKDCollege@users.noreply.github.com>
2022-12-31 15:40:02 +0000

    Solved

--- set04/problem01.c
+++ set04/problem01.c
@@ -8,7 +8,6 @@ int main()
 {
   int num1,den1,num2,den2,res_num,res_den;
   input(&num1,&den1,&num2,&den2);
-  printf("Entered Fractions are %d/%d and %d/%d\n",num1,den1,num2,den2);
   add(num1,den1,num2,den2,&res_num,&res_den);
   output(num1,den1,num2,den2,res_num,res_den);
   return 0;
@@ -18,8 +17,10 @@ void input(int *num1, int *den1, int *num2, int *den2)
 {
   printf("Enter Numerator-1 and Denominator-1\n");
   scanf("%d%d",num1,den1);
+  printf("Entered Fraction is %d/%d\n",*num1,*den1);
   printf("Enter Numerator-2 and Denominator-2\n");
   scanf("%d%d",num2,den2);
+  printf("Entered Fraction is %d/%d\n",*num2,*den2);
 }
 
 void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
@@ -38,18 +39,13 @@ void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
   int prod;
   prod=den1*den2;
   lcm=prod/hcf;
-  if (den1==lcm && den2==lcm)
-  {
-    num1=num1*1;
-    num2=num2*1;
-  }
-  else if (den1!=lcm){num1=num1*(lcm/den1);}
-  else if (den2!=lcm){num2=num2*(lcm/den2);}
+  num1=num1*(lcm/den1);
+  num2=num2*(lcm/den2);
   *res_num=num1+num2;
   *res_den=lcm;
 }
 
 void output(int num1, int den1, int num2, int den2, int res_num, int res_den)
 {
-  printf("%d/%d + %d/%d = %d/%d\n",num1,den1,num2,den2,res_num,res_den);
+  printf("\n%d/%d + %d/%d = %d/%d\n",num1,den1,num2,den2,res_num,res_den);
 }
---
+++ set04/problem02.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+struct fract
+{
+  int num,den;
+};
+typedef struct fract Fraction;
+
+Fraction input_fraction();
+Fraction compare(Fraction a, Fraction b, Fraction c);
+void output(Fraction a, Fraction b, Fraction c, Fraction smallest);
+
+int main()
+{
+  Fraction x,y,z,smallest;
+  x=input_fraction();
+  y=input_fraction();
+  z=input_fraction();
+  smallest=compare(x,y,z);
+  output(x,y,z,smallest);
+  return 0;
+}
+
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("Enter Numerator and Denominator Respectively\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+}
+
+Fraction compare(Fraction a, Fraction b, Fraction c)
+{
+  float d=0,e=0,f=0;
+  d=(a.num*1.0)/(a.den*1.0); 
+  e=(b.num*1.0)/(b.den*1.0);
+  f=(c.num*1.0)/(c.den*1.0);
+  //Multiplied with 1.0 to make a.num(int) as a float.
+  if (d<=e && d<=f){return a;}
+  else if (e<=d && e<=f){return b;}
+  else if (f<=d && f<=e){return c;}
+}
+
+void output(Fraction a, Fraction b, Fraction c, Fraction smallest)
+{
+  printf("The Smallest among %d/%d, %d/%d and %d/%d is %d/%d\n",a.num,a.den,b.num,b.den,c.num,c.den,smallest.num,smallest.den);
+}
