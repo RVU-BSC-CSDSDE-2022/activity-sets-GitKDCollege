e16216bcd891096a445868665922084cca48d1fb
Kishor  Desai <114481961+GitKDCollege@users.noreply.github.com>
2023-01-04 05:50:36 +0000

    Solved

---
+++ set04/problem04.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<math.h>
+
+int input_degree();
+void input_coefficients(int n, float a[n]);
+float input_x();
+float evaluate_polynomial(int n, float a[n], float x);
+void output(int n, float a[n], float x, float result);
+
+int main()
+{
+  int highest_degree;
+  highest_degree=input_degree();
+  float coeff[highest_degree];
+  input_coefficients(highest_degree,coeff);
+  float x;
+  x=input_x();
+  float result;
+  result=evaluate_polynomial(highest_degree,coeff,x);
+  output(highest_degree,coeff,x,result);
+  return 0;
+}
+
+int input_degree()
+{
+  int n;
+  printf("Enter the Highest Degree of 'x'\n");
+  scanf("%d",&n);
+  return n;
+}
+
+void input_coefficients(int n, float a[n])
+{
+  for (int i=0;i<=n;i++)
+    {
+      printf("Enter Co-Efficient for x^%d \n",i);
+      scanf("%f",&a[i]);
+    }
+}
+
+float input_x()
+{
+  float x;
+  printf("Enter the Value of 'x'\n");
+  scanf("%f",&x);
+  return x;
+}
+
+float evaluate_polynomial(int n, float a[n], float x)
+{
+  float sum=0;
+  for (int i=0;i<=n;i++)
+    {
+      sum=sum+(a[i]*(pow(x,i)));
+    }
+  return sum;
+}
+
+void output(int n, float a[n], float x, float result)
+{
+  for (int i=0;i<=n;i++)
+    {
+      if (i<=n-1)
+      {
+        printf("%.2f * %.2f^%d + ",a[i],x,i);
+      }
+      else
+      {
+        printf("%.2f * %.2f^%d",a[i],x,i);
+      }
+    }
+  printf(" = %.2f\n",result);
+}
